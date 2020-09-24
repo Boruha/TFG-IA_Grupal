f8f3b0e73bc69abdd834c6b81cf50993aca0d7bc
Borja Pozo Wals <borja.pozo@gmail.com>
2020-09-24 20:17:33 +0200

    Primer commit. Adición makefile v1.0, tinyPTC y main de prueba

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <main.hpp>
+
+extern "C"
+{
+  #include "tinyPTC/tinyptc.h"  
+} 
+
+int main()
+{
+
+    ptc_open("window", 640, 360);
+
+    for(;;);
+
+    ptc_close();
+
+    return 0;
+}
